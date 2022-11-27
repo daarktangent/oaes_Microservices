bc3220fcbe35ca2afb145033a7ea857521641023
daarktangent <aman.jha0409@gmail.com>
2022-11-28 00:23:20 +0530

    oaes added

--- src/main/java/com/example/oaes/Controller/ExamPatternController.java
+++ src/main/java/com/example/oaes/Controller/ExamPatternController.java
@@ -3,8 +3,11 @@ package com.example.oaes.Controller;
 import com.example.oaes.Dao.*;
 
 import com.example.oaes.Entity.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -15,56 +18,30 @@ public class ExamPatternController {
 
     @Autowired
     private ExamPatternDao examPatternDao;
-    @GetMapping("/")
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @GetMapping("/exam/get")
     public List<ExamPattern> getAll()
     {
         return examPatternDao.findAll();
     }
-    @PostMapping("/add")
+    @PostMapping("/exam/add")
     public void addExamPattern(@RequestBody ExamPattern examPattern)
     {
         examPatternDao.save(examPattern);
     }
 
-    @Autowired
-    private McqsDao mcqsDao;
-    @Autowired
-    private MsqsDao msqsDao;
 
-    @Autowired
-    private QuestionPaperDao questionPaperDao;
-    @Autowired
-    private DescriptiveDao descriptiveDao;
-
-    @GetMapping("/generate/{id}")
-    public QuestionPaper generate(@PathVariable Long id)
+    @GetMapping("/exam/test")
+    public String test()
     {
-        QuestionPaper questionPaper=new QuestionPaper();
-        ExamPattern examPattern=examPatternDao.getExamPatternByPid(id);//getting the exam pattern
-        System.out.println("----------------------");
-        System.out.println(examPattern);
-        List<Mcqs> mcq=mcqsDao.findAll();
-        List<Msqs> msq=msqsDao.findAll();
-        List<Descriptive> descriptives= descriptiveDao.findAll();
-        Collections.shuffle(descriptives);
-        Collections.shuffle(msq);
-        Collections.shuffle(mcq);
-        System.out.println(examPattern+"--------a----------");
-        for(int i=0;i<examPattern.getNo_of_mcqs();i++)
-        {
-            questionPaper.addMcq(mcq.get(i));
-        }
-        for(int i=0;i<examPattern.getNo_of_desc();i++)
-        {
-            questionPaper.addDesc(descriptives.get(i));
-        }
-        for (int i=0;i<examPattern.getNo_of_msqs();i++)
-        {
-            questionPaper.addMsqs(msq.get(i));
-        }
-        System.out.println(questionPaper);
-        questionPaperDao.save(questionPaper);
-        return questionPaper;
+        return "inside exam pattern test microservice";
+    }
+    @GetMapping("exam/gen/{id}")
+    public String generate(@PathVariable Long id)
+    {
+        return this.restTemplate.getForObject("http://GENERATE-SERVICE/generate/"+id,String.class);
     }
 
 }
--- src/main/java/com/example/oaes/OaesApplication.java
+++ src/main/java/com/example/oaes/OaesApplication.java
@@ -2,12 +2,23 @@ package com.example.oaes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+
 public class OaesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OaesApplication.class, args);
 	}
 
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
 }
